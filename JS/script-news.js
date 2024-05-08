302143f8157ded63504e801fd8414ad753b20edd
chacharin <102975545+chacharin@users.noreply.github.com>
2024-05-08 16:18:56 +0700

    Add files via upload

--- JS/script-news.js
+++ JS/script-news.js
@@ -12,22 +12,36 @@ function fetchDataAndRender() {
             // Get the card container
             var cardContainer = document.getElementById('card-container');
             
-            // Loop through the last 3 data entries
-            lastThreeData.forEach((entry, index) => {
-                // Create a new card element
-                var cardDiv = document.createElement('div');
-                cardDiv.classList.add('col-md-4');
-                cardDiv.innerHTML = `
-                    <div class="card mb-3">
-                        <img src="${entry.pic1}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">${entry.topic}</h5>
-                            <p class="card-text">${entry.intro}</p>
-                        </div>
-                    </div>
-                `;
-                cardContainer.appendChild(cardDiv);
-            });
+// Loop through the last 3 data entries
+lastThreeData.forEach((entry, index) => {
+    // Create a new card element
+    var cardDiv = document.createElement('div');
+    cardDiv.classList.add('col-md-4');
+    
+    // Determine the link based on the loop index
+    var link;
+    if (index === 0) {
+        link = './0news1.html';
+    } else if (index === 1) {
+        link = './0news2.html';
+    } else if (index === 2) {
+        link = './0news3.html';
+    }
+    
+    cardDiv.innerHTML = `
+        <div class="card mb-3">
+            <a href="${link}">
+                <img src="${entry.pic1}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${entry.topic}</h5>
+                    <p class="card-text">${entry.intro}</p>
+                </div>
+            </a>
+        </div>
+    `;
+    cardContainer.appendChild(cardDiv);
+});
+
         })
         .catch(error => {
             console.error('Error fetching data:', error);
